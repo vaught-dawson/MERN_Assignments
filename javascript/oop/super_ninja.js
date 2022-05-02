38687c5f0487b3feed8b02339f7823a86dfce6f6
Dawson Vaught <vaught.dawson@gmail.com>
2022-05-02 16:42:37 -0700

    add a new file super_ninja.js

---
+++ javascript/oop/super_ninja.js
@@ -0,0 +1,19 @@
+const Ninja = require("./ninja.js");
+
+class Sensei extends Ninja {
+  constructor(name) {
+    super(name, 200, 10, 10);
+    this.wisdom = 10;
+  }
+
+  speakWisdom() {
+    super.drinkSake();
+    console.log(
+      "What one programmer can do in one month, two programmers can do in two months."
+    );
+  }
+}
+
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+superSensei.showStats();
