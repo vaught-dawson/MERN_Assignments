2bae9038c467f1360b67af5e2fd4231bfecf3e97
Dawson Vaught <vaught.dawson@gmail.com>
2022-05-16 11:23:50 -0700

    moved server files into server directory

--- full_stack_mern/product_manager_1/server/server.js
+++ full_stack_mern/product_manager_1/server/server.js
@@ -2,16 +2,16 @@ const fs = require("fs");
 const express = require("express");
 const cors = require("cors");
 
-require("./server/config/mongoose.config");
+require("./config/mongoose.config");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-fs.readdirSync("./server/routes")
+fs.readdirSync(`${__dirname}/routes`)
   .filter((file) => file.endsWith(".routes.js"))
-  .map((route) => require(`./server/routes/${route}`)(app));
+  .map((route) => require(`./routes/${route}`)(app));
 
 app.listen(5000, () =>
   console.log("[Event] Express server is listening on port 5000")
