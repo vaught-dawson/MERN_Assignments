9e21a579b5b28a6c2e9e854e1b5d8775908a6170
Dawson Vaught <vaught.dawson@gmail.com>
2022-05-18 12:27:41 -0700

    added validations

--- full_stack_mern/refacter_reuse_recycle/server/models/product.model.js
+++ full_stack_mern/refacter_reuse_recycle/server/models/product.model.js
@@ -4,12 +4,18 @@ const ProductSchema = new mongoose.Schema(
   {
     title: {
       type: String,
+      required: [true, "Title is required"],
+      minlength: [1, "Title must have at least 1 character"],
     },
     price: {
       type: Number,
+      required: [true, "Price is required"],
+      min: [0, "Price can not go below zero"],
     },
     description: {
       type: String,
+      required: [true, "Description is required"],
+      minlength: [1, "Description must have at least 1 character"],
     },
   },
   { timestamps: true }
